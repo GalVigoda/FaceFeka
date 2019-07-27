aec9bd157d8b7aa22d2344327b0ab1492d16cba8
GalVigoda <37512127+GalVigoda@users.noreply.github.com>
2019-07-27 23:06:09 +0300

    Add files via upload

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 
 <head>
     <title>FaceFeka</title>
+    <link rel = "icon" href ="/img/facebookLogo.JPG" type = "image/x-icon">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
@@ -11,8 +12,8 @@
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-            <img src="/img/facebookLogo1.JPG" width="120" height="50" href="index.php">
                 <a class="navbar-brand" href="index.php">FaceFeka</a>
+                <!--<img src="/img/facebookLogo2.JPG" width="120" height="50" href="index.php">!-->
             </div>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="dashboard.php">Home</a></li>
